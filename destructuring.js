e0e9ab30e0c97f19cd0121de581e7746c5233b85
BRian Burke <suspectsdpa@gmail.com>
2022-04-21 22:31:08 -0600

    committing the destructuring exercise

---
+++ destructuring.js
@@ -0,0 +1,12 @@
+// 1. 8 - 1846
+// 2. 1846, 1659
+// 3. your name is Alejandro and you like purple // your name is Melissa and you like green
+// 4. maya, marisa, chi
+// 5. raindrops on roses, whiskers on kittens, bright copper kettles warm woolen mittens brown paper packages tied up with strings
+// 6. 10, 30, 20
+
+const {a, b} = obj.numbers
+
+[1,2] = [2,1];
+
+const raceResults = ([first, second, third, ...rest]) => ({first, second, third, rest})
